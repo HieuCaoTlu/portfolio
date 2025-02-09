c47855ac5b8bcc0d1b34bb7680c64b26cb693315
Cao Trung Hiếu <153081121+HieuCaoTlu@users.noreply.github.com>
2025-02-09 15:44:45 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ function App() {
   return (
     <main>
       <h1>Cao Trung Hieu</h1>
-      <h5>Không ngại nhìn mèo ướt đâu</h5>
-      <p>Nhưng mà thôi CTH sẽ đợi cậu tắm xong</p>
     </main>
   )
 }
